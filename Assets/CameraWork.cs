99134680cba92316cd4017912a33415587b093ae
Sebeom Lee <moses97@gmail.com>
2019-10-06 15:04:01 -0400

    First commit

---
+++ Assets/CameraWork.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWork : MonoBehaviour
+{
+    public Transform pivot;
+
+    private void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(pivot.position, pivot.up, 0.5f * Input.gyro.rotationRateUnbiased.y);
+        transform.RotateAround(pivot.position, transform.right, 0.5f * Input.gyro.rotationRateUnbiased.x);
+        transform.position = pivot.position - transform.forward * 10f;
+    }
+}
